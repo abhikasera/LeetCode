531876259501c4cf1ac8b23f9056d639780ed2c3
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2024-08-30 23:38:58 +0530

    Time: 68 ms (10.91%), Space: 40.4 MB (13.93%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        ListNode temp = head;
+        
+        int count = 0;
+        while(temp != null)
+        {
+            count++;
+            temp = temp.next;
+        }
+        
+        int mid = count/2;
+        temp = head;
+        while(mid > 0){
+            temp = temp.next;
+            mid--;
+        }
+        
+        Console.WriteLine(temp.val);
+        
+        return temp;
+    }
+}
