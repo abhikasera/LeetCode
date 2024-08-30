d91cb0a035ab3a82fa2adf170566e238519d0819
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2024-08-30 23:30:31 +0530

    Time: 80 ms (52.46%), Space: 49.5 MB (5.34%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int DiameterOfBinaryTree(TreeNode root) {
+        if(root == null)
+            return 0;
+        
+        int currentMax = 0;
+        
+        MaxHeight(root, ref currentMax);
+        
+        return currentMax;
+    }
+    
+    public int MaxHeight(TreeNode root, ref int currentMax){
+        if(root == null)
+            return 0;
+        
+        int left = MaxHeight(root.left, ref currentMax);
+        int right = MaxHeight(root.right, ref currentMax);
+        
+        if(currentMax < left + right)
+            currentMax = left + right;
+        
+        Console.WriteLine(root.val + " - " + left + " - " + right + " - " + currentMax);
+        
+        return Math.Max(left,  right) + 1;
+    }
+}
