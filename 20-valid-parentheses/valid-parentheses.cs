32af2e345e1664e248efec1ed4e8c84de03ca3b9
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2025-06-18 03:29:46 +0530

    Time: 2 ms (81.96%) | Memory: 42.3 MB (37.10%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool IsValid(string s) {
+
+        List<char> stack = new List<char>();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(s[i] == '[' || s[i] == '{' || s[i] == '(')
+                stack.Add(s[i]);
+            else
+            {
+                if(!stack.Any())
+                    return false;
+                
+                char e = stack.Last();
+                stack.RemoveAt(stack.Count - 1);
+                if(s[i] == ']' && e != '[')
+                    return false;
+                else if(s[i] == '}' && e != '{')
+                    return false;
+                else if(s[i] == ')' && e != '(')
+                    return false;
+
+            }
+        }
+        if(stack.Any())
+            return false;
+
+        return true;
+    }
+}
