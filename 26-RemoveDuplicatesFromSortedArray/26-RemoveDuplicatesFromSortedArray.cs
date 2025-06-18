a8a4e786f3577932121c5c0cd9168e43d528e44f
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2025-06-18 22:41:21 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,22 @@
+// Last updated: 6/18/2025, 10:41:21 PM
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        
+        int k = 1;
+        List<int> elements = new List<int>() { nums[0] };
+        
+        for(int i = 1; i < nums.Length; i++){
+            if(elements.Last() != nums[i])
+            {
+                elements.Add(nums[i]);
+            }
+        }
+        
+        for(int i = 0; i < elements.Count(); i++)
+        {
+            nums[i] = elements[i];
+        }
+        
+        return elements.Count();
+    }
+}
