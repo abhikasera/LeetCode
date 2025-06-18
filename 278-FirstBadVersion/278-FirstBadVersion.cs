b0a7db11540a5d4d7840189010faf7a5f1e73ce2
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2025-06-18 22:40:54 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.cs
@@ -0,0 +1,29 @@
+// Last updated: 6/18/2025, 10:40:55 PM
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        if(n < 2 && IsBadVersion(1))
+            return 1;        
+        return FindBadVersion(1, n);
+            
+    }
+    
+    public int FindBadVersion(int min, int max)
+    {   
+        //Console.Write($"{min} - {max} - ");
+        
+        if(min >= max)
+            return min;
+        
+        int mid = ((max - min)/2 + min);
+        bool isBad = IsBadVersion(mid);
+        //Console.WriteLine($"{mid} - {isBad.ToString()}");
+        
+        if(isBad)
+           return FindBadVersion(min, mid);
+        else
+           return FindBadVersion(mid+1, max);
+    }
+}
