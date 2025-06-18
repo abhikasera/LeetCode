99232df910689923c9b5fbee86cc156de4b6f63e
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2025-06-18 22:40:53 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.cs
@@ -0,0 +1,24 @@
+// Last updated: 6/18/2025, 10:40:53 PM
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        Dictionary<char, int> countMap = new Dictionary<char, int>();
+        
+        for(int i = 0; i < magazine.Length; i++)
+        {
+            if(countMap.ContainsKey(magazine[i]))
+                countMap[magazine[i]] += 1;
+            else
+                countMap[magazine[i]] = 1;                
+        }
+        
+        for(int i = 0; i < ransomNote.Length; i++)
+        {
+            if(countMap.ContainsKey(ransomNote[i]) && countMap[ransomNote[i]] > 0)
+                countMap[ransomNote[i]] -= 1;
+            else
+                return false;                
+        }
+        
+        return true;
+    }
+}
