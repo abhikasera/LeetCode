5649e6a7da61da09789523a6bebf6a38570978e6
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2025-06-18 22:40:51 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.cs
@@ -0,0 +1,29 @@
+// Last updated: 6/18/2025, 10:40:52 PM
+public class Solution {
+    public int LongestPalindrome(string s) {
+        Dictionary<char, int> count = new Dictionary<char, int>(); 
+        for(int i = 0; i < s.Length; i++){
+            if(count.ContainsKey(s[i]))
+                count[s[i]] = count[s[i]] + 1;
+            else
+                count[s[i]] = 1;
+        }
+        
+        int maxLength = 0;
+        bool doesNonEvenExist = false;
+        foreach(var x in count.Keys){
+            
+            if(count[x]/2 >= 0){
+                maxLength = maxLength + (count[x]/2) * 2;
+            }
+                
+            if(count[x]%2 != 0)
+                doesNonEvenExist = true;
+        }
+        
+        if(doesNonEvenExist)
+            maxLength +=1;
+    
+        return maxLength;
+    }
+}
