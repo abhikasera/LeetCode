02f1a3ec878ce16372758b882331faaab4bf3a9f
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2025-06-18 22:41:07 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cs
@@ -0,0 +1,43 @@
+// Last updated: 6/18/2025, 10:41:08 PM
+public class Solution {
+    public bool IsPalindrome(string s) {
+        
+        bool isPalindrome = true;
+        
+        for(int i = 0, j = s.Length - 1;i <= j; i++, j--)
+        {
+            while(i < s.Length && !IsAlphaNumeric(s[i])){
+                i++;
+            }
+            
+            while(j > 0 && !IsAlphaNumeric(s[j])){
+                j--;
+            }
+            
+            if(i <= j && Char.ToLower(s[i]) == Char.ToLower(s[j]))
+            {
+                continue;
+            }
+            else if(i > j)
+            {
+                break;
+            }
+            else
+            {
+                isPalindrome = false;
+            }
+        }
+        
+        return isPalindrome;
+        
+    }
+    
+    public bool IsAlphaNumeric(char x)
+    {
+        x = Char.ToLower(x);
+        if((x >=48 && x <= 57) || (x >= 97 && x <= 122))
+            return true;
+        
+        return false;
+    }
+}
