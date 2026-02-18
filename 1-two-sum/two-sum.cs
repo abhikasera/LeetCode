18fc584a8ba0106c478af45009a9bf4f794b6487
Abhishek Kasera <37591864+abhikasera@users.noreply.github.com>
2026-02-19 01:55:53 +0530

    Time: 2 ms (55.69%) | Memory: 50.1 MB (5.27%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,23 +1,17 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> map =  new Dictionary<int, int>();
-
-        map[nums[0]] = 0;
-
+        Dictionary<int, int> hashmap =  new Dictionary<int, int>();
+        hashmap[nums[0]] = 0;
         int[] output = new int[2];
 
         int x = 0;
-        for(int i = 1; i < nums.Length; i++)
-        {
+        for(int i = 1; i < nums.Length; i++){
             x = target - nums[i];
-            if(map.ContainsKey(x))
-            {
-                output[0] = map[x];
+            if(hashmap.ContainsKey(x)){
+                output[0] = hashmap[x];
                 output[1] = i;
-                break;
             }
-
-            map[nums[i]] = i;
+            hashmap[nums[i]] = i;
         }
 
         return output;
